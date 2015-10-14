bc783d7d34dacd856b65d52ff860c0319ad481ae
bimalaksmana <bima.laksmana@gmail.com>
2015-10-14 19:21:23 +0700

    First push

---
+++ files/social_map_alpha_ver_/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,[i],j,_(k,l,m,n,o,p,q,_(),r,_(s,t,u,v,w,_(x,y,z,A),B,null,C,D,E,D,F,G,H,null,I,J,K,L,M,N,O,J),P,_(),Q,_(),R,_(S,[_(T,U,V,W,m,X,Y,X,Z,ba,r,_(bb,_(bc,bd,be,bf)),P,_(),bg,_(bh,bi,bj,_(bk,bl,bm,bn,bo,[]),bp,g))])),bq,_(),br,_(bs,_(bt,bu)));}; 
+var b="url",c="social_map_alpha_ver_.html",d="generationDate",e=new Date(1444824599775.07),f="isCanvasEnabled",g=false,h="variables",i="OnLoadVariable",j="page",k="packageId",l="e3ae9991381a42e9be2536906542d349",m="type",n="Axure:Page",o="name",p="Social map Alpha ver.",q="notes",r="style",s="baseStyle",t="627587b6038d43cca051c114ac41ad32",u="pageAlignment",v="center",w="fill",x="fillType",y="solid",z="color",A=0xFFFFFFFF,B="image",C="imageHorizontalAlignment",D="near",E="imageVerticalAlignment",F="imageRepeat",G="auto",H="favicon",I="sketchFactor",J="0",K="colorStyle",L="appliedColor",M="fontName",N="Applied Font",O="borderWidth",P="adaptiveStyles",Q="interactionMap",R="diagram",S="objects",T="id",U="0f7034002a9e4bd98cd852b4206c9a92",V="label",W="",X="inlineFrame",Y="styleType",Z="visible",ba=true,bb="size",bc="width",bd=1440,be="height",bf=768,bg="target",bh="targetType",bi="webUrl",bj="urlLiteral",bk="exprType",bl="stringLiteral",bm="value",bn="map.html",bo="stos",bp="includeVariables",bq="masters",br="objectPaths",bs="0f7034002a9e4bd98cd852b4206c9a92",bt="scriptId",bu="u0";
+return _creator();
+})());
